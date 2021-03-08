5789f3f31e32f9f0297c3a2741b9bf9915b4a89c
Suliat Adams <sadams@vectra.ai>
2021-03-08 11:31:57 +0000

    fixed errors and logic

--- src/user-lambda.py
+++ src/user-lambda.py
@@ -2,16 +2,18 @@ import boto3
 import json
 import logging
 import os
-import bcrypt
+# import bcrypt
 
 from datetime import datetime
 
 logger = logging.getLogger()
 dynamodb = boto3.resource("dynamodb", region_name="eu-west-1")
-table = dynamodb.Table(os.getenv("TABLE_NAME", "USERS"))
+table = dynamodb.Table(os.getenv("TABLE_NAME", "USER"))
 now = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z"
 
-# bcrypt.hashpw(password.encode(), bcrypt.gensalt()),    
+# def validate_password(saved_password, password):
+#     match = bcrypt.checkpw(password.encode(), saved_password) 
+#     return match
 
 def create_user(event):
     SUPPORTED_METHODS = ["POST"]
@@ -20,7 +22,6 @@ def create_user(event):
     email = (event["body"]).get("email")
     username = (event["body"]).get("name")
     secret = (event["body"]).get("password")
-
     if method not in SUPPORTED_METHODS:
         logger.error(
             f"Unsupported method {method}. Path {path} only supports {str(SUPPORTED_METHODS)}"
@@ -30,25 +31,39 @@ def create_user(event):
             "headers": {"Content-Type": "application/json"},
             "body": "Allow: POST",
         }
-    if not email or secret or username:
+    if not (email or secret or username):
         logger.error("Missing User data")
         return {
             "statusCode": 400,
             "headers": {"Content-Type": "application/json"},
             "body": "Required: Username, Email, Password",
         }
-    user_dict={
-        "email": email,
-        "username": username,
-        "password": secret,
-        "last_login": now
-    }
-    table.put_item(Item = user_dict)
-    return{
-        "statusCode": 201,
-        "headers": {"Content-Type": "application/json"},
-        "body": f"User: {username} Created Successfully",
-    }
+    user = table.get_item(
+        Key={
+            "email":email
+        }
+    )
+    
+    if not user.get("Item"):
+        
+        user_dict={
+            "email": email,
+            "username": username,
+            "password": secret,
+            "last_login": now
+        }
+        table.put_item(Item = user_dict)
+        return{
+            "statusCode": 201,
+            "headers": {"Content-Type": "application/json"},
+            "body": f"User: {username} Created Successfully",
+        }
+    else: 
+        return {
+            "statusCode": 403,
+            "headers": {"Content-Type": "application/json"},
+            "body": "Validation Error, Email unavailable"
+        }
 
     
 def update_user(event):
@@ -66,9 +81,9 @@ def update_user(event):
         return {
             "statusCode": 405,
             "headers": {"Content-Type": "application/json"},
-            "body": "Allow: POST",
+            "body": "Allow: PATCH",
         }
-    if not email or secret or new_secret:
+    if not (email or secret or new_secret):
         logger.error("Missing User data")
         return {
             "statusCode": 400,
@@ -76,18 +91,16 @@ def update_user(event):
             "body": "Required: Username, Email, Password",
         }
     response = table.update_item(
-        Key={"email": email, "password": secret},
+        Key={"email": email},
         UpdateExpression="set password=:p",
         ExpressionAttributeValues={":p": new_secret},
         ReturnValues="UPDATED_NEW",
     )
-
-    return(
-        return(
+    return { 
         "statusCode": 204,
         "headers": {"Content-Type": "application/json"},
-        "body": f"Password Updated for {email}")
-    )
+        "body": f"Password Updated for {email}"
+    }
     
 def delete_user(event):
     SUPPORTED_METHODS = ["DELETE"]
@@ -105,23 +118,36 @@ def delete_user(event):
             "headers": {"Content-Type": "application/json"},
             "body": "Allow: DELETE",
         }
-    if not email or secret:
+    if not (email or secret):
         logger.error("Missing User data")
         return {
             "statusCode": 400,
             "headers": {"Content-Type": "application/json"},
             "body": "Required: Username, Email, Password",
         }
-    table.delete_item(
+    user = table.get_item(
         Key={
-            "email": email,
-            "password": secret
+            "email":email
         }
     )
-    return(
+    if not response.get("Item"):
+        logger.error("User not found")
+        return {
+            "statusCode": 404,
+            "headers": {"Content-Type": "application/json"},
+            "body": "Not Found"
+        }
+    if response.get("Item")["password"] == secret:
+        table.delete_item(
+            Key={
+                "email": email
+            }
+        )
+    return {
         "statusCode": 204,
         "headers": {"Content-Type": "application/json"},
-        "body": f"User with email {email} has been Deleted")
+        "body": f"User with email {email} has been Deleted"
+    }
     
     
 def login(event):
@@ -143,7 +169,7 @@ def login(event):
             "headers": {"Content-Type": "application/json"},
             "body": "Allow: POST",
         }
-    if not email or secret:
+    if not (email or secret):
         logger.error("Missing User data")
         return {
             "statusCode": 400,
@@ -153,11 +179,10 @@ def login(event):
     logger.info("Getting user from database")
     response = table.get_item(
         Key={
-            "email":email,
-            "password": secret
+            "email":email
         }
     )
-    if not response["Item"]:
+    if not response.get("Item"):
         logger.error("User not found")
         return {
             "statusCode": 404,
@@ -171,24 +196,30 @@ def login(event):
         return {
             "statusCode": 200,
             "headers": {"Content-Type": "application/json"},
-            "body": json.dumps(response["Items"]),
+            "body": json.dumps(response["Item"]),
+        }
+    else:
+        logger.error("validation Error")
+        return {
+            "statusCode": 403,
+            "headers": {"Content-Type": "application/json"},
+            "body": "Validation Error"
         }
     
-def handler(event, context):
+def lambda_handler(event, context):
     """Lambda entrypoint"""
     logger.info(json.dumps(event))
-
     # Paths subject to change
-    if event["path"] == "user/create":
+    if event["path"] == "/user/create":
         return create_user(event)
         
-    elif event["path"] == "user/login":
+    elif event["path"] == "/user/login":
         return login(event)
     
-    elif event["path"] == "user/update":
+    elif event["path"] == "/user/update":
         return update_user(event)
     
-    elif event["path"] == "user/delete":
+    elif event["path"] == "/user/delete":
         return delete_user(event)
     else:
         logger.error(f"Path {event['path']} not found")
